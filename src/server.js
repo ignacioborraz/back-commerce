e3ca4e7fe86d8832f19edeae4f42e35066f03360
ignacioborraz <ignacioborraz@hotmail.com>
2023-05-09 14:20:30 -0300

    fix

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import router from './routes/index.js'
 import error_handler from './middlewares/error_handler.js'
 import not_found_handler from './middlewares/not_found.js'
 import { engine } from 'express-handlebars'
-import { __dirname } from '../utils.js'
+import { __dirname } from './utils.js'
 
 const server = express()
 
