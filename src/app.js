36882b12283fc4d07e484345a3e431eecb5f3e5c
ignacio.borraz <ignacio.borraz@koonolmexico.com>
2023-08-14 20:18:36 -0300

    morgan

--- src/app.js
+++ src/app.js
@@ -5,10 +5,12 @@ import router from './routes/index.js'
 import error_handler from './middlewares/error_handler.js'
 import not_found_handler from './middlewares/not_found.js'
 import { __dirname } from './utils.js'
+import morgan from 'morgan'
 
 const server = express()
 
 //middlewares
+server.use(morgan('dev'))
 server.use('',express.static('public'))
 server.use('/img',express.static('img'))
 server.use(express.json())
