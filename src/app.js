3d2b3f54789950317b81d6a196133c8b93046754
ignacio.borraz <ignacio.borraz@koonolmexico.com>
2023-08-14 20:06:30 -0300

    reset

--- src/app.js
+++ src/app.js
@@ -5,18 +5,10 @@ import router from './routes/index.js'
 import error_handler from './middlewares/error_handler.js'
 import not_found_handler from './middlewares/not_found.js'
 import { __dirname } from './utils.js'
-import cookieParser from 'cookie-parser'
-import expressSession from 'express-session'
 
 const server = express()
 
 //middlewares
-server.use(cookieParser(process.env.SECRET_COOKIE))
-server.use(expressSession({
-    secret: process.env.SECRET_SESSION,
-    resave: true,
-    saveUninitialized: true
-}))
 server.use('',express.static('public'))
 server.use('/img',express.static('img'))
 server.use(express.json())
@@ -26,7 +18,7 @@ server.use(error_handler)
 server.use(not_found_handler)
 
 //database
-connect('mongodb+srv://igna:hola1234@cluster0.dbl4oxi.mongodb.net/commerce') //requiere mínimo un parámetro: el link de conexión (URI)
+connect(process.env.LINK_DB) //requiere mínimo un parámetro: el link de conexión (URI)
     .then(()=>console.log('database connected'))
     .catch(err=>console.log(err))
 
--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -2,15 +2,11 @@ import { Router } from "express"
 import auth_router from "./auth.js"
 import products_router from "./products.js"
 import carts_router from "./carts.js"
-import cookies_router from "./cookies.js"
-import sessions_router from "./sessions.js"
 
 const router = Router()
 
 router.use('/auth',auth_router)
 router.use('/products',products_router)
 router.use('/carts',carts_router)
-router.use('/cookies',cookies_router)
-router.use('/sessions',sessions_router)
 
 export default router
